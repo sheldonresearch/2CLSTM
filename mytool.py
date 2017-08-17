396a87b31955c989c7c89086d0ec66435612012b
sunxiangguo <sunxiangguodut@qq.com>
2017-08-17 11:23:49 +0800

    Update mytool.py

--- mytool.py
+++ mytool.py
@@ -9,12 +9,12 @@
 @site: http://blog.csdn.net/github_36326955
 @software: PyCharm
 @file: mytool
-@time: 17-8-15 下午1:35
+@time: 17-8-15 1:35pm
 """
 import os
 import sys
 import numpy as np
-from sklearn.metrics import precision_score,accuracy_score
+from sklearn.metrics import precision_score
 
 
 GLOVE_DIR = "/home/sunxiangguo/PycharmProjects/glove.6B/"
@@ -24,8 +24,8 @@ def get_embeddings():
     with open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt')) as f:
         for line in f:
             values = line.split()
-            word = values[0]  # 单词的字符表示
-            coefs = np.asarray(values[1:], dtype='float32')  # 单词的向量表示
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
             embeddings_index[word] = coefs
     print('Found %s word vectors.' % len(embeddings_index))
     return embeddings_index
@@ -58,32 +58,13 @@ def load_data_labels(TEXT_DATA_DIR):
                     labels.append(label_id)
     return texts, labels, labels_index
 
-def log_results(path, accuracy, micro_precision, weighted_precision, macro_precision):
+def log_results(path, macro_precision):
     with open(path, "a") as f:
-        f.write("accuracy  for classification model - " + str(accuracy))
-        f.write("\n")
-        f.write("micro Precision score for classification model - ")
-        f.write(str(micro_precision))
-        f.write("\n")
-        f.write("weighted Precision score for classification model - ")
-        f.write(str(weighted_precision))
-        f.write("\n")
         f.write("macro Precision score for classification model - ")
         f.write(str(macro_precision))
         f.write("\n\n")
 
 
 def get_result(y_test, y_pred):
-    accuracy = accuracy_score(y_test, y_pred)
-    print("accuracy  for classification model - ", accuracy)
-
-    micro_precision = precision_score(y_test, y_pred, average='micro')
-    print("micro Precision score for classification model - ", micro_precision)
-
-    weighted_precision = precision_score(y_test, y_pred, average='weighted')
-    print("weighted Precision score for classification model - ", weighted_precision)
-
     macro_precision = precision_score(y_test, y_pred, average='weighted')
     print("macro Precision score for classification model - ", macro_precision)
-
-    return accuracy, micro_precision, weighted_precision, macro_precision
